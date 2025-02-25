69f638c0cfde26c661eec6c3745870f5d9fde195
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-02-25 13:24:26 -0500

    move faster + double jump

--- consumables/buffs.lua
+++ consumables/buffs.lua
@@ -10,29 +10,31 @@ SMODS.ConsumableType({
         collection = "Buffs",
         undiscovered = {
             name = "Undiscovered Buff",
-            text = {
-                "Find this buff",
-                "in a pack to unlock"
-            }
+            text = {"Find this buff", "in a pack to unlock"}
         }
-    },
+    }
 })
 
 function use_buff(self, card, area, copier)
     if card.ability.extra.chips then -- Add chips to cards
         for _, _card in ipairs(G.hand.cards) do
-            _card.ability.perma_bonus = (_card.ability.perma_bonus or 0) + card.ability.extra.chips
+            _card.ability.perma_bonus = (_card.ability.perma_bonus or 0) +
+                                            card.ability.extra.chips
             _card:juice_up()
         end
     elseif card.ability.extra.damage_mult then -- Add damage to weapons
         for _, weapon in ipairs(G.weapons.cards) do
-            weapon.ability.extra.min_damage = weapon.ability.extra.min_damage * card.ability.extra.damage_mult
-            weapon.ability.extra.max_damage = weapon.ability.extra.max_damage * card.ability.extra.damage_mult
+            weapon.ability.extra.min_damage =
+                weapon.ability.extra.min_damage * card.ability.extra.damage_mult
+            weapon.ability.extra.max_damage =
+                weapon.ability.extra.max_damage * card.ability.extra.damage_mult
             weapon:juice_up()
         end
     elseif card.ability.extra.ammo_mult then -- Add ammo to weapons
         for _, weapon in ipairs(G.weapons.cards) do
-            weapon.ability.extra.max_ammo = math.ceil(weapon.ability.extra.max_ammo * card.ability.extra.ammo_mult)
+            weapon.ability.extra.max_ammo = math.ceil(
+                                                weapon.ability.extra.max_ammo *
+                                                    card.ability.extra.ammo_mult)
             weapon.ability.extra.curr_ammo = weapon.ability.extra.max_ammo
             weapon:juice_up()
         end
@@ -59,17 +61,14 @@ function Buff(info)
                 damage_mult = info.damage_mult or nil,
                 ammo_mult = info.ammo_mult or nil,
                 in_pack = info.in_pack or false,
-                type = info.type or 1, -- 1 can only be used in round
+                type = info.type or 1 -- 1 can only be used in round
             }
         },
-        loc_txt = {
-            name = info.name,
-            text = info.text or {"A buff."}
-        },
+        loc_txt = {name = info.name, text = info.text or {"A buff."}},
         calculate = info.calculate or nil,
         use = info.use or use_buff,
         can_use = info.can_use or can_use_buff,
-        unlocked = true,
+        unlocked = true
     })
     table.insert(FVB.cards, 'c_fvb_' .. info.key)
 end
@@ -79,11 +78,8 @@ Buff({
     name = "Medkit",
     atlas = "othercards",
     pos = {x = 10, y = 1},
-    text = {
-        "Add {C:blue}+50{} Chips",
-        "to every card in hand"
-    },
-    chips = 50,
+    text = {"Add {C:blue}+50{} Chips", "to every card in hand"},
+    chips = 50
 })
 
 Buff({
@@ -91,10 +87,7 @@ Buff({
     name = "Small Head",
     atlas = "othercards",
     pos = {x = 1, y = 0},
-    text = {
-        "Reduce {C:attention}Blind{} size",
-        "by {C:mult}50%{}",
-    },
+    text = {"Reduce {C:attention}Blind{} size", "by {C:mult}50%{}"},
     use = function(self, card, area, copier)
         G.GAME.blind.chips = math.floor(G.GAME.blind.chips * 0.5)
         G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
@@ -119,7 +112,7 @@ Buff({
         "{C:mult}damage{} by {C:white,X:mult}50%{}"
     },
     damage_mult = 1.5,
-    type = 0,
+    type = 0
 })
 
 Buff({
@@ -129,11 +122,10 @@ Buff({
     pos = {x = 4, y = 1},
     in_pack = true,
     text = {
-        "Increase {C:weapon}Weapon's",
-        "{C:inactive}ammo{} by {C:green}50%{}"
+        "Increase {C:weapon}Weapon's", "{C:inactive}ammo{} by {C:green}50%{}"
     },
     ammo_mult = 1.5,
-    type = 0,
+    type = 0
 })
 
 Buff({
@@ -143,8 +135,7 @@ Buff({
     atlas = "othercards",
     pos = {x = 8, y = 2},
     text = {
-        "Duplicate the {C:weapon}Weapon",
-        "you currently have",
+        "Duplicate the {C:weapon}Weapon", "you currently have",
         "{C:inactive}(Doesn't stack)"
     },
     use = function(self, card, area, copier)
@@ -153,10 +144,8 @@ Buff({
         G.weapons:emplace(_card)
         G.weapons.cards[2].ability = table.copy(G.weapons.cards[1].ability)
     end,
-    can_use = function(self, card)
-        return #G.weapons.cards == 1
-    end,
-    type = 0,
+    can_use = function(self, card) return #G.weapons.cards == 1 end,
+    type = 0
 })
 
 Buff({
@@ -164,15 +153,43 @@ Buff({
     name = "Health Up",
     atlas = "othercards",
     pos = {x = 3, y = 0},
-    text = {
-        "Adds {C:blue}+10{} Chips",
-        "per card in hand",
-        "to each card"
-    },
+    text = {"Adds {C:blue}+10{} Chips", "per card in hand", "to each card"},
     use = function(self, card, area, copier)
         for _, _card in ipairs(G.hand.cards) do
-            _card.ability.perma_bonus = (_card.ability.perma_bonus or 0) + 10 * #G.hand.cards
+            _card.ability.perma_bonus = (_card.ability.perma_bonus or 0) + 10 *
+                                            #G.hand.cards
             _card:juice_up()
         end
+    end
+})
+
+Buff({
+    key = "move_faster",
+    name = "Move Faster",
+    atlas = "othercards",
+    pos = { x = 5, y = 0 },
+    text = {"Reduces Blind size", "by {C:attention}10%"},
+    use = function(self, card, area, copier)
+        G.GAME.blind.chips = math.floor(G.GAME.blind.chips * 0.9)
+        G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+
+        local chips_UI = G.hand_text_area.blind_chips
+        G.FUNCS.blind_chip_UI_scale(G.hand_text_area.blind_chips)
+        G.HUD_blind:recalculate()
+        chips_UI:juice_up()
+
+        play_sound('chips2')
+    end
+})
+
+Buff({
+    key = "double_jump",
+    name = "Double Jump",
+    atlas = "othercards",
+    pos = { x = 6, y = 0},
+    text = {"{C:attention}Sets scored chips to blind size", "{C:mult}-2{} hand size"},
+    use = function (self, card, area, copier)
+        G.GAME.chips = G.GAME.blind.chips
+        G.hand:change_size(-2)
     end
 })
