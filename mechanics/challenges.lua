578fc0907f4e14176b9ed758f3419fe5e9103337
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-02-27 14:29:59 -0500

    changes challenge

--- mechanics/challenges.lua
+++ mechanics/challenges.lua
@@ -12,6 +12,9 @@ SMODS.Challenge({
             {
                 id = 'consumable_slots',
                 value = 7
+            },
+            {
+                id = "no_shop_jokers"
             }
         },
         custom = {
@@ -35,6 +38,22 @@ SMODS.Challenge({
     restrictions = {
         banned_other = {
             {id = "bl_final_leaf", type = "blind"}
+        },
+        banned_cards = {
+            {id = "p_buffoon_normal_1", ids = {
+                "p_buffoon_normal_2",
+                "p_buffoon_jumbo_1",
+                "p_buffoon_mega_1"
+            }},
+        },
+        banned_tags = {
+            {id = "tag_uncommon"},
+            {id = "tag_rare"},
+            {id = "tag_negative"},
+            {id = "tag_foil"},
+            {id = "tag_holo"},
+            {id = "tag_polychrome"},
+            {id = "tag_top_up"},
         }
     },
     deck = {
