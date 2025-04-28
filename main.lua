740d65938ec1d408fcd3f0cc9ba971b54bd64815
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-04-28 14:30:00 -0400

    imported the hand level card

--- main.lua
+++ main.lua
@@ -36,3 +36,33 @@ SMODS.current_mod.optional_features = function()
         },
     }
 end
+
+---Levels up hand (with proper animation) [from JoyousSpring]
+---@param card Card|table
+---@param hand_key string
+---@param instant boolean?
+---@param amount integer?
+function rae_level_up_hand(card, hand_key, instant, amount)
+    if not instant then
+        update_hand_text({
+            sound = "button",
+            volume = 0.7,
+            pitch = 0.8,
+            delay = 0.3
+        }, {
+            handname = localize(hand_key, "poker_hands"),
+            chips = G.GAME.hands[hand_key].chips,
+            mult = G.GAME.hands[hand_key].mult,
+            level = G.GAME.hands[hand_key].level
+        })
+    end
+    level_up_hand(card, hand_key, instant, amount)
+    if not instant then
+        update_hand_text({
+            sound = "button",
+            volume = 0.7,
+            pitch = 1.1,
+            delay = 0
+        }, {mult = 0, chips = 0, handname = "", level = ""})
+    end
+end
--- testing.lua
+++ testing.lua
@@ -1,11 +1,11 @@
 return {
-    key = "b_fvb_fossil_deck",
-    calculate = function(self, card, context) 
-		if context.weapon_hit and 
-			SMODS.has_enhancement(context.other_card, 'm_stone') then
-				eventify(function() context.other_card:flip() delay(0.5) end)
-				eventify(function() context.other_card:set_ability(G.P_CENTERS.c_base) end)
-				eventify(function() delay(0.75) context.other_card:flip() end)
-		end
-	end,
+    key = "j_fvb_friends_movie",
+    calculate = function (self, card, context)
+        if context.retrigger_joker_check and context.other_context.cardarea == G.weapons then
+            print("we made it")
+            return {
+                repetitions = 1
+            }
+        end
+    end
 }
